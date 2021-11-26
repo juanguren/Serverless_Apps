018cc4ba8e32020400ea7e60a51e581340476c77
Juan Felipe <juan_fe_7@live.com>
2021-11-26 07:55:42 -0500

    Changed the endpoint's method to POST, in order to send dates as JSON. Included a validation for the body.

--- financial_modeling_prep_wrapper/index.ts
+++ financial_modeling_prep_wrapper/index.ts
@@ -8,11 +8,14 @@ import { validateTickerExists } from './src/controllers/api_calls/validators';
   retrieveHistoricalData
 );*/
 
-api.get('/data/historical/:ticker', async (req, res) => {
+api.post('/data/historical/:ticker', async (req, res) => {
   let ticker = req.params.ticker;
   try {
+    if (!req.body.time_range)
+      throw { code: 404, message: 'Empty body, please include a time_range' };
     await retrieveHistoricalData(req, res, ticker);
   } catch (error) {
-    return res.status(500).json(error);
+    const { code, message } = error;
+    return res.status(code).json({ message });
   }
 });
