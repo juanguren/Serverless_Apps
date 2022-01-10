5a569b4280c3152a70bcbb5727cf397cafeafa77
Juan Felipe <juan_fe_7@live.com>
2022-01-10 17:59:51 -0500

    Installed dotenv and created the test case for the GET endpoint

--- global_storage/tests/integration/index.test.ts
+++ global_storage/tests/integration/index.test.ts
@@ -1,4 +1,10 @@
 import { api } from '@serverless/cloud';
+import request from 'supertest';
+import dotenv from 'dotenv';
+import { validGetResponse } from './mocks';
+//import { jest } from '@jest/globals';
+
+dotenv.config();
 
 test("should return a 'Healthy' 200 status", async () => {
   const response = await api.get('/').invoke();
@@ -8,4 +14,18 @@ test("should return a 'Healthy' 200 status", async () => {
 
   /*expect(body).toHaveProperty('users');
   expect(body.users.length).toBeGreaterThan(0);*/
+});
+
+describe('Main', () => {
+  const content = { 'Content-Type': 'application/json' };
+  const { API_KEY_TEST } = process.env;
+
+  it('Should correctly retrieve data', async () => {
+    const response = await request(api)
+      .get('/data/testKey')
+      .set({ api_key: API_KEY_TEST });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toMatchObject(validGetResponse);
+  });
 });
---
+++ global_storage/tests/integration/mocks.ts
@@ -0,0 +1,12 @@
+const validGetResponse = {
+  data: {
+    test: 'Once moree',
+    test2: [
+      {
+        user: 'Juan',
+      },
+    ],
+  },
+};
+
+export { validGetResponse };
