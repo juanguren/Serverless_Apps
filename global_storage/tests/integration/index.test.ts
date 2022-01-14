8b9f6f0c9694ecd5095dab96ca0e6c57f48aad99
Juan Felipe <juan_fe_7@live.com>
2022-01-13 20:40:24 -0500

    Defined the app-wide test cases and strengthened the DELETE method test

--- global_storage/tests/integration/index.test.ts
+++ global_storage/tests/integration/index.test.ts
@@ -10,14 +10,53 @@ import {
 
 dotenv.config();
 
-test("should return a 'Healthy' 200 status", async () => {
-  const response = await api.get('/').invoke();
+describe('Application-Wide', () => {
+  const { API_KEY_TEST } = process.env;
+  const headers = { api_key: API_KEY_TEST };
 
-  expect(response.status).toBe(200);
-  expect(response.body).toMatchObject({ message: 'Healthy' });
+  test("should return a 'Healthy' 200 status", async () => {
+    const response = await api.get('/').invoke();
+
+    expect(response.status).toBe(200);
+    expect(response.body).toMatchObject({ message: 'Healthy' });
+  });
+
+  test('Should return a 404 for incorrect route or missing params', async () => {
+    const badRoute = '/hey';
+    const expectedResponse = {
+      error: 'Incorrect route or missing params',
+    };
+
+    const response = await api
+      .get(`/data/${getKey()}/${badRoute}`)
+      .invoke(undefined, { headers });
+
+    const responseTwo = await api
+      .get(`/${badRoute}/${getKey()}`)
+      .invoke(undefined, { headers });
+
+    expect(response.status).toBe(404);
+    expect(response.body).toMatchObject(expectedResponse);
+    expect(responseTwo.status).toBe(404);
+    expect(responseTwo.body).toMatchObject(expectedResponse);
+  });
+
+  test('Should respond with an unauthorized error in case incorrect header is used', async () => {
+    const badKey = { api_key: 'HEY' };
+    const expectedResponse = {
+      message: 'Incorrect token. User is not authenticated',
+    };
+
+    const response = await api
+      .get(`/data/${getKey()}`)
+      .invoke(undefined, { headers: badKey });
+
+    expect(response.status).toBe(403);
+    expect(response.body).toMatchObject(expectedResponse);
+  });
 });
 
-describe('Main', () => {
+describe('Main Endpoints', () => {
   const { API_KEY_TEST } = process.env;
   const headers = { api_key: API_KEY_TEST };
   const content = { 'Content-Type': 'application/json' };
@@ -30,20 +69,6 @@ describe('Main', () => {
 
       expect(response.status).toBe(200);
       expect(response.body).toMatchObject(validGetResponse);
-    });
-
-    it('Should respond with an unauthorized error in case incorrect header is used', async () => {
-      const badKey = { api_key: 'HEY' };
-      const expectedResponse = {
-        message: 'Incorrect token. User is not authenticated',
-      };
-
-      const response = await api
-        .get(`/data/${getKey()}`)
-        .invoke(undefined, { headers: badKey });
-
-      expect(response.status).toBe(403);
-      expect(response.body).toMatchObject(expectedResponse);
     });
 
     it('Should return a 404 error when searching by a non-existant key', async () => {
@@ -103,6 +128,15 @@ describe('Main', () => {
 
       expect(response.status).toBe(200);
       expect(response.body).toHaveProperty('message');
+    });
+
+    afterAll(async () => {
+      const data = await api
+        .get(`/data/${keyName}`)
+        .invoke(undefined, { headers });
+
+      expect(data.status).toBe(404);
+      expect(data.body).toHaveProperty('message');
     });
   });
 });
