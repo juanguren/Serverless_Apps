dcce88e6aa76963765eb96e2e78a22c323b049d6
Juan Felipe <juan_fe_7@live.com>
2022-02-17 21:08:24 -0500

    supposedly env variables

--- crypto/src/(de)tokenizer.ts
+++ crypto/src/(de)tokenizer.ts
@@ -4,15 +4,17 @@ import { createHmac, createCipheriv, createDecipheriv } from "crypto";
 const SECRET_KEY = "a1b2c3d4";
 const KEY = "encrypted account number";
 const IV = "555s5d55f5OPP__)";
+const HASHING_ALGORITHM = "sha256";
+const CIPHER_DECIPHER_ALGORITHM = "aes-192-ccm";
 
 const encryptUserData = (secureData: IUserSecure): IUserSecure => {
   const { accountNumber, password } = secureData;
 
-  const passwordHashed = createHmac("sha256", SECRET_KEY) // Creates an HMC object configured with the algorithm and the used key
+  const passwordHashed = createHmac(HASHING_ALGORITHM, SECRET_KEY) // Creates an HMC object configured with the algorithm and the used key
     .update(password) // updates it (append?) with the data to-be-hashed
     .digest("hex"); // This appears to parse the HMC object back to a string-like format
 
-  const cipher = createCipheriv("aes-192-ccm", KEY, IV);
+  const cipher = createCipheriv(CIPHER_DECIPHER_ALGORITHM, KEY, IV);
   let encryptedAccount = cipher.update(String(accountNumber), "utf8", "hex");
   encryptedAccount += cipher.final("hex");
 
@@ -24,7 +26,7 @@ const encryptUserData = (secureData: IUserSecure): IUserSecure => {
 
 const decryptUserData = (secureData: IUserSecure): IUserSecure => {
   const { accountNumber } = secureData;
-  const decipher = createDecipheriv("aes-192-ccm", KEY, IV);
+  const decipher = createDecipheriv(CIPHER_DECIPHER_ALGORITHM, KEY, IV);
   let decryptedAccountNumber = decipher.update(accountNumber, "hex", "utf8");
 
   decryptedAccountNumber += decipher.final("utf8");
