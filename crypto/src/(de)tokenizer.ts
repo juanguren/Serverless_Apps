ee50e27a4f3f426103f7a58bff0fbfaea4962bb7
Juan Felipe <juan_fe_7@live.com>
2022-02-24 20:59:33 -0500

    Included commentary

--- crypto/src/(de)tokenizer.ts
+++ crypto/src/(de)tokenizer.ts
@@ -1,11 +1,14 @@
 import { IUserSecure } from "./interfaces";
 import { createHmac, createCipheriv, createDecipheriv } from "crypto";
 
+// ! Hashing
 const SECRET_KEY = "a1b2c3d4";
-const KEY = "encrypted account number";
-const IV = "555s5d55f5OPP__)";
-const HASHING_ALGORITHM = "sha256";
-const CIPHER_DECIPHER_ALGORITHM = "aes192";
+const HASHING_ALGORITHM = "sha256"; // special algo 1
+
+// ! Chiper-Decipher
+const KEY = "encrypted account number"; // Private Key
+const IV = "555s5d55f5OPP__)"; // Nonce or: random number used in combination with a secret key as a means to encrypt data
+const CIPHER_DECIPHER_ALGORITHM = "aes192"; // // special algo 1
 
 const encryptUserData = (secureData: IUserSecure): IUserSecure => {
   const { accountNumber, password } = secureData;
@@ -14,9 +17,12 @@ const encryptUserData = (secureData: IUserSecure): IUserSecure => {
     .update(password) // updates it (append?) with the data to-be-hashed
     .digest("hex"); // This appears to parse the HMC object back to a string-like format
 
-  const cipher = createCipheriv(CIPHER_DECIPHER_ALGORITHM, KEY, IV);
+  const cipher = createCipheriv(CIPHER_DECIPHER_ALGORITHM, KEY, IV); // Cipher object
+
+  // Feeds data to the cipher object **** update(DATA, DATATYPE, OUTPUT)
   let encryptedAccount = cipher.update(String(accountNumber), "utf8", "hex");
-  encryptedAccount += cipher.final("hex");
+
+  encryptedAccount += cipher.final("hex"); // Completes the process somehow?
 
   return {
     accountNumber: encryptedAccount,
