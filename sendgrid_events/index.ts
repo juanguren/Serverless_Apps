6d864489af725a2ff3e33a85577abaf41c065f9b
Juan Felipe <juan_fe_7@live.com>
2022-03-06 18:30:22 -0500

    Linted the controller

--- sendgrid_events/index.ts
+++ sendgrid_events/index.ts
@@ -1,12 +1,8 @@
 import { api, Request, Response } from '@serverless/cloud';
 import { userRegistration } from './middlewares/user.service';
 
-api.get('/main', (req: Request, res: Response) => {
-  res.send('OK');
-});
+api.get('/main', (req: Request, res: Response) => res.send('OK'));
 
 api.get('/signup', userRegistration);
 
-api.get('/*', (req, res) => {
-  res.redirect('/main');
-});
+api.get('/*', (req: Request, res: Response) => res.redirect('/main'));
