4515f903139fee1cc9a3d16905e5b5bffd7ecc1c
Juan Felipe <juan_fe_7@live.com>
2022-03-05 20:22:49 -0500

    Created the sendgrid and user services

---
+++ sendgrid_events/middlewares/user.service.ts
@@ -0,0 +1,29 @@
+import { data, Request, Response } from '@serverless/cloud';
+import { throwError } from '../helpers/error.helper';
+import { nanoid } from 'nanoid';
+import { LoggerFactory } from '../factories/abstract/logger-class';
+
+const logger = LoggerFactory.createLogger();
+
+export const userRegistration = async (
+  req: Request,
+  res: Response,
+) => {
+  try {
+    const user = req.body;
+    const id = nanoid();
+    user['id'] = id;
+
+    const createdUser = await data.set(`${id}`, {
+      user,
+      createdAt: new Date().toISOString(),
+    });
+
+    logger.info(`User ${id} registered`);
+
+    res.send({ createdUser });
+  } catch (error) {
+    logger.error({ error });
+    throwError(req, res, { error });
+  }
+};
--- sendgrid_events/services/sendgrid.service.ts
+++ sendgrid_events/services/sendgrid.service.ts
@@ -1,25 +1,36 @@
-import {
-  data,
-  params,
-  events,
-  Request,
-  Response,
-} from '@serverless/cloud';
+import { data, params } from '@serverless/cloud';
 import mail from '@sendgrid/mail';
-import { throwError } from '../helpers/error.helper';
+import { ISendGrid } from '../middlewares/types';
+import { LoggerFactory } from '../factories/abstract/logger-class';
 
 const { SENDGRID_KEY } = params;
+const logger = LoggerFactory.createLogger();
 
-export const handleEmailOperations = async (
-  req: Request,
-  res: Response,
-) => {
+export const sendEmail = async (recipient: string) => {
   try {
     if (SENDGRID_KEY) {
       mail.setApiKey(SENDGRID_KEY);
+
+      const emailObject: ISendGrid = {
+        to: recipient,
+        from: 'juanararo@unisabana.edu.co',
+        subject: '',
+        text: '',
+        html: '',
+      };
+      await mail.send(emailObject);
+
+      logger.info(`Email sent to ${recipient}`);
+
+      return { message: 'Message sent', recipient };
     } else {
-      throwError(req, res, { message: 'No sendgrid key', code: 404 });
+      throw {
+        message: 'No sendgrid key found',
+        code: 404,
+      };
     }
-    const emailContent = req.body;
-  } catch (error) {}
+  } catch (error) {
+    logger.error({ error });
+    return error;
+  }
 };
