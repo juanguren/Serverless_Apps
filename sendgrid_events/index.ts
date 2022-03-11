2565de7a894382bbc52e7868d971b83e2e4b696a
Juan Felipe Aranguren <juan.aranguren@globant.com>
2022-03-11 18:03:43 -0500

    Included Promises experiments

--- sendgrid_events/index.ts
+++ sendgrid_events/index.ts
@@ -1,8 +1,11 @@
 import { api, Request, Response } from '@serverless/cloud';
 import { userRegistration } from './middlewares/user.service';
+import getJsonApiData from './middlewares/json-promise.service';
 
 api.get('/main', (req: Request, res: Response) => res.send('OK'));
 
 api.post('/signup', userRegistration);
+
+api.get('/promises', getJsonApiData);
 
 api.get('/*', (req: Request, res: Response) => res.redirect('/main'));
