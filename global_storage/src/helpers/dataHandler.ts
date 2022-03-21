160302e01f805c8f89d053e802356eba469a3598
Juan Felipe <juan_fe_7@live.com>
2022-03-20 22:14:48 -0500

    helpers

---
+++ global_storage/src/helpers/dataHandler.ts
@@ -0,0 +1,38 @@
+import { data, Request, Response } from '@serverless/cloud';
+import {
+  AcceptedData,
+  DataAction,
+} from '../interfaces/dataInterfaces';
+
+const postDataHandler = async (
+  req: Request,
+  res: Response,
+  dataSet: AcceptedData,
+  action: DataAction,
+) => {
+  let {
+    keyName = 'key',
+    overwrite = undefined,
+    timeToLive = undefined,
+  } = dataSet.instructions;
+  const { content } = dataSet;
+  const { api_key } = req.headers;
+
+  try {
+    const option = { overwrite, timeToLive };
+    const useOptions =
+      Object.entries(option).length === 0 ? undefined : option;
+    content.token = api_key;
+    await data.set(keyName, content, useOptions);
+
+    return res.status(200).json({
+      message: action,
+      keyName,
+    });
+  } catch (error) {
+    const code = error.statusCode || 500;
+    return res.status(code).json(error);
+  }
+};
+
+export { postDataHandler };
