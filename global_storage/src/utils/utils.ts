18c6f14c8aad1533accc76b26ccc735638ad4a7f
Juan Felipe Aranguren <juan.aranguren@globant.com>
2022-05-22 11:18:06 -0500

    Moved in a new utils method

--- global_storage/src/utils/utils.ts
+++ global_storage/src/utils/utils.ts
@@ -1,6 +1,11 @@
+import { data } from '@serverless/cloud';
+
 const removeTokenFromPayload = (payload: any) => {
   delete payload.token;
   return payload;
 };
 
-export { removeTokenFromPayload };
+const getExistingKey = async (key: string): Promise<any> =>
+  await data.get(key);
+
+export { removeTokenFromPayload, getExistingKey };
